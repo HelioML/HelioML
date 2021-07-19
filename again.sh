cfd693b3335c1bbb19f4cecc5b8f8f5d37f56cc9
James Paul Mason <jmason86@gmail.com>
2021-07-19 12:45:03 -0600

    Removed script for testing environment files

--- again.sh
+++
@@ -1,8 +0,0 @@
-source ~/anaconda3/etc/profile.d/conda.sh
-conda activate helioml
-conda remove --name helioml_ch3 --all
-rm -rf /Users/jmason86/Library/Jupyter/kernels/helioml_ch3
-conda env create -f environment_ch3.yml
-conda activate helioml_ch3
-python -m ipykernel install --user --name=helioml_ch3
-jupyter lab
